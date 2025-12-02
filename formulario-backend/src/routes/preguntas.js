9d6d3f0f5dcfb6ddeb3377f60454ad7f6ae8835e
gruezomen <haewodk@gmail.com>
2025-12-02 19:27:52 -0400

    corrección de error con la ruta al middleware

--- formulario-backend/src/routes/preguntas.js
+++ formulario-backend/src/routes/preguntas.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/preguntas');
 const { validatePregunta } = require('../validators/preguntas');
-const { verificarToken, soloProfesor } = require('../middleware/authmiddleware');
+const { verificarToken, soloEditor } = require('../middleware/authmiddleware');
 
 // Rutas públicas
 router.get('/', ctrl.obtenerPreguntas);
 router.get('/:id', ctrl.obtenerPreguntaPorId);
 
 // Rutas protegidas (ELIMINA las antiguas, deja solo estas)
-router.post('/', verificarToken, soloProfesor, validatePregunta, ctrl.crearPregunta);
-router.put('/:id', verificarToken, soloProfesor, validatePregunta, ctrl.actualizarPregunta);
-router.delete('/:id', verificarToken, soloProfesor, ctrl.eliminarPregunta);
+router.post('/', verificarToken, soloEditor, validatePregunta, ctrl.crearPregunta);
+router.put('/:id', verificarToken, soloEditor, validatePregunta, ctrl.actualizarPregunta);
+router.delete('/:id', verificarToken, soloEditor, ctrl.eliminarPregunta);
 
 module.exports = router;
